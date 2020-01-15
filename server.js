7745d4d0fc8fc7e272bb37e0d2003710a7f48473
Glitch (vivacious-crustacean) <none>
2020-01-15 00:34:02 +0000

    💗🚆 Checkpoint ./server.js:5890191/84

--- server.js
+++ server.js
@@ -166,11 +166,11 @@ app.post("/sign-up", function(request, response) {
                                             bcrypt.hash(request.body["password"], saltRounds, function(err, hash) {
                                             // create a user
                                                 let obj = {inspname: request.body["inspname"], email: request.body["email"], password: hash, city: "", street: "", books: [], income: [], outcome: []};
-                                                let user = new reportModel(obj);
+                                                let user = new userModel(obj);
                                                 user.save(function (err) {
                                                   if (!err) console.log('Success!');
                                                       // login after registration
-                                                      reportModel.find({inspname: request.body["inspname"], email: request.body["email"], password: hash}, function (err, document) {
+                                                      userModel.find({inspname: request.body["inspname"], email: request.body["email"], password: hash}, function (err, document) {
                                                         if(!err) {
                                                           let user_id = document[0]["id"];
                                                           request.login(user_id, () => {
@@ -196,7 +196,7 @@ app.post("/sign-up", function(request, response) {
 });
 /***********************************/
 app.post("/log-in", function(request, response) {
-              reportModel.find({ email: request.body["email"]}, function (err, document) {
+  userModel.find({ email: request.body["email"]}, function (err, document) {
               if(!err) {
                 if(document.length == 0) {
                   response.json({"error": "error0"});
@@ -228,7 +228,7 @@ app.post("/log-out", function(request, response) {
 app.post("/is-loged-in", function(request, response) {
   // in addition to check is loged in user also we get user inspname
   if(request.session.hasOwnProperty("passport")) {
-   reportModel.findById(request.session.passport.user, (err, document) => {
+   userModel.findById(request.session.passport.user, (err, document) => {
      if(!err) {
        response.json({isLogedIn: request.isAuthenticated(), inspname: document.inspname, city: document.city, street: document.street, books: document.books, income: document.income, outcome: document.outcome});
      } 
