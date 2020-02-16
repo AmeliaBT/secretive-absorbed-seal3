9f881b870006fa805a602f6047160bac89edf348
Glitch (mousy-tiara) <none>
2020-02-16 17:38:10 +0000

    🍮⛰ Checkpoint ./app/components/RIedit.jsx:5890191/6576

--- public/bundle.js
+++ public/bundle.js
@@ -58998,7 +58998,6 @@ const Link = __webpack_require__(12).Link
 const Header = __webpack_require__(23);
 // react-bootstrap
 const {Grid, Row, Col, FormControl, ControlLabel, FormGroup, HelpBlock, Tabs, Tab, Form, Button} = __webpack_require__(14);
-
 class RIedit extends React.Component {
   constructor(props) {   
     super(props);
@@ -59257,107 +59256,7 @@ React.createElement(FormGroup, { className: "input-row"}, React.createElement(C
    
     /* 
         
-  <Form  method="post" action="/addRIreport" onSubmit={this.handleSubmit} enctype="multipart/form-data" >       
- <Grid>  
-
-   <Row>  <Col smOffset={7}  sm={4} > 
-      <FormGroup > 
-     <Button className="btn btn-primary btn-block" type="submit"><i className="fa fa-paper-plane"></i> Submit</Button>
-      </FormGroup>
-</Col></Row>
-   
-<Row >
-<Col sm={3} >	 <div className="well">   
- <FormGroup className="input-row"><ControlLabel>Inspector:</ControlLabel> <FormControl   readOnly type="text" name="inspector" value={this.state.inspector	} placeholder={this.state.inspector	 }  /> </FormGroup>    
-
-  <FormGroup className="input-row"><ControlLabel>Supplier:</ControlLabel>    <FormControl  className="input-row" type="text" name="ewo" value={this.state.ewo}   onChange={this.handleChangeValue} />  </FormGroup>  
  
-  </div></Col>  
-  <Col sm={2} >	 <div className="well">    
-   <FormGroup ><ControlLabel>WO / PO / MTT:</ControlLabel>
-   
-     <FormControl 
-      className="input-row3" type="text" name="dwo" 
-       value={this.state.dwo} 
-       onChange={this.handleChangeValue}> 
-     </FormControl>  </FormGroup> 
-    
-  
-  <FormGroup className="input-row"><ControlLabel> 	 NO:</ControlLabel> <FormControl type="text" name="kwo"       value={this.state.kwo	} placeholder={this.state.kwo	} onChange={this.handleChangeValue} />  </FormGroup> 
- </div></Col>
- ----------------------------------------
-<Col sm={3} >	<div className="well"> 
-  <FormGroup className="input-row"><ControlLabel>Date Received:	</ControlLabel> <FormControl type="date" name="cwo"      value={this.state.cwo	} placeholder={this.state.cwo	} onChange={this.handleChangeValue} />  </FormGroup> 
-  <FormGroup className="input-row"><ControlLabel> Date Inspected: </ControlLabel><FormControl type="date" name="jwo"   value={this.state.jwo	} placeholder={this.state.jwo	} onChange={this.handleChangeValue} />  </FormGroup> 
-  </div></Col>
-<Col sm={3} >	<div className="well"> 
-<FormGroup ><ControlLabel>Source:</ControlLabel> 
-  <FormControl  className="input-row3" type="text" name="mwo" value={this.state.mwo}  onChange={this.handleChangeValue}>  </FormControl> </FormGroup> 
-<FormGroup ><ControlLabel>Destination:</ControlLabel> <FormControl  className="input-row3" type="text" name="nwo" value={this.state.nwo}  onChange={this.handleChangeValue}> </FormControl> </FormGroup> 
-  </div></Col>
-  </Row>
- <Row> <Col  sm={11} >  <div className="profile-line"></div></Col>  </Row>   
-   <Row>
-     
-
-<Col sm={4} ><div className="well">   
-<FormGroup className="input-row"><ControlLabel> 	 P/N:	</ControlLabel><FormControl type="text" name="fwo"   value={this.state.fwo	} placeholder={this.state.fwo	} onChange={this.handleChangeValue} />  </FormGroup> 
-  </div></Col>
-<Col sm={4} ><div className="well">   
-<FormGroup className="input-row"><ControlLabel> 	 Description:	</ControlLabel><FormControl type="text" name="Gwo"   value={this.state.Gwo	} placeholder={this.state.Gwo	} onChange={this.handleChangeValue} />  </FormGroup> 
-  </div></Col>
-
-   <Col sm={3} ><div className="well">    
-   
-  <FormGroup className="input-row"><ControlLabel> 	 Received SW:	</ControlLabel><FormControl type="text" name="iwo"   value={this.state.iwo	} placeholder={this.state.iwo	} onChange={this.handleChangeValue} />  </FormGroup> 
-  </div></Col>
-  </Row>
-   
-   
- <Row> <Col  sm={11} >  <div className="profile-line"></div></Col>  </Row>
-
-  <Row>
-  <Col sm={2} >	   <div className="well">
- <FormGroup className="input-row"><ControlLabel> 	 Lot Size:	</ControlLabel><FormControl type="number" name="owo"   value={this.state.owo	} placeholder={this.state.owo	} onChange={this.handleChangeValue} />  </FormGroup> 
-  <FormGroup  className="input-row"><ControlLabel> 	 Sample Size:	</ControlLabel><FormControl type="number" name="pwo"   value={this.state.pwo	} placeholder={this.state.pwo	} onChange={this.handleChangeValue} />  </FormGroup> 
- </div></Col>
-
- <Col sm={2} >	   <div className="well">
-<FormGroup className="input-row"><ControlLabel> 	 Qty Defective:	</ControlLabel><FormControl type="number" name="qwo"   value={this.state.qwo	} placeholder={this.state.qwo	} onChange={this.handleChangeValuePF} />  </FormGroup> 
-    <FormGroup className="input-row"><ControlLabel> 	 Qty Rejected:	</ControlLabel><FormControl type="number" name="rwo"   value={this.state.rwo	} placeholder={this.state.rwo	} onChange={this.handleChangeValue} />  </FormGroup> 
- </div></Col>
-     <Col sm={2} >	   <div className="well">
-<FormGroup className="input-row"><ControlLabel> 	 Pass / Fail:	</ControlLabel><FormControl type="text" name="two"   value={this.state.two	} placeholder={this.state.two	} onChange={this.handleChangeValue}   />  </FormGroup> 
-<FormGroup className="input-row"><ControlLabel> 	 DMR #:	</ControlLabel><FormControl type="text" name="swo"   value={this.state.swo	} placeholder={this.state.swo	} onChange={this.handleChangeValue} />  </FormGroup> 
-</div></Col>
-
-  <Col sm={5} > 
- <FormGroup className="input-row"><ControlLabel > Comment:</ControlLabel> 
-  <textarea rows="3" type="textarea"  className="input-rowC" name="record"   value={this.state.record	} placeholder={this.state.record	} onChange={this.handleChangeValue}  /> 
-  </FormGroup> 
-   </Col>
-  </Row>
- <Row> <Col  sm={11} >  <div className="profile-line"></div></Col>  </Row>
- <Row> 
-<Col sm={6} >
-<FormGroup className="input-row"><ControlLabel> 	Photo link	</ControlLabel><FormControl type="file"  name="lwo"    value={this.state.lwo }  placeholder={this.state.lwo	} onChange={this.handleChangeValue}  />   </FormGroup>  
-</Col>
- <Col sm={5} >
-<FormGroup className="input-row"><ControlLabel> 	Photo extra:	</ControlLabel><FormControl type="file" name="uwo"   value={this.state.uwo	} placeholder={this.state.uwo	} onChange={this.handleChangeValue} />  </FormGroup> 
- </Col> 
-</Row>  
-   
-  </Grid>     
-   </Form>  
-        <Col  sm={5} >	
-  <FormGroup className="input-row"><ControlLabel > 	 PN:	</ControlLabel><FormControl type="text" name="fwo"   value={this.state.fwo	} placeholder={this.state.fwo	} onChange={this.fwoChanged} />  </FormGroup> 
-  <FormGroup className="input-row" ><ControlLabel > 	Description :	</ControlLabel><FormControl type="text" name="Gwo"   value={this.state.Gwo	} placeholder={this.state.Gwo	} onChange={this.GwoChanged} />  </FormGroup> 
-  <FormGroup className="input-row" ><ControlLabel> 	 Date:	</ControlLabel><FormControl type="text" name="jwo"   value={this.state.jwo	} placeholder={this.state.jwo	} onChange={this.jwoChanged} />  </FormGroup> 
-  <div className="profile-line"></div>
-        </Col> 
-        <br/>
-   
-   
    
         */
         
