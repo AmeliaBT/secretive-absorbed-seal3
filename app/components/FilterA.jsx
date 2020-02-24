cace26961b64a5690295e61d9b37479798c75614
Glitch (mousy-tiara) <none>
2020-02-24 23:07:03 +0000

    🏤🎂 Checkpoint ./app/components/FilterA.jsx:5890191/479

---
+++ app/components/FilterA.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+const Link = require('react-router-dom').Link
+// style for BOOKS
+const style = require('../styles/FormRI');
+// other components and etc
+const Header = require('./Header');
+//const CreatableSelect= require('react-select');
+//const { Creatable }= require('react-select');
+
+// react-bootstrap
+const {Filter, ResponsiveEmbed, Image, InputGroup, option, Form, FormGroup, Col, FormControl, Button, Grid, Row, ControlLabel} = require('react-bootstrap');
