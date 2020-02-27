d79cff335bb8e6c7e2b1116271e1a8069de9778c
Glitch (mousy-tiara) <none>
2020-02-27 23:03:51 +0000

    ❤️🚋 Checkpoint ./app/components/SidebarA.jsx:5890191/1817

--- app/components/SidebarA.jsx
+++ app/components/SidebarA.jsx
@@ -1,14 +1,16 @@
-/
+/* ****************************************
+Sidebar component that displays a list of options, and receives a callback function as its props. The callback function will be called from the Sidebar component by clicking one of the buttons, and will allow to notify the containing component (the parent) of the click
+*************************************************** */
 const React = require('react');
 const Link = require('react-router-dom').Link
-// style for BOOKS
-//const style = require('../styles/FormRI');
-// other components and etc
+
+
+const {Radio, Col, Grid, Row, Button, Glyphicon , OverlayTrigger, Popover} = require('react-bootstrap');
+
+const style = require('../styles/RIlistItem');
 const options =require('./exampleData');
 //const CustomizedTypeahead =require('./CustomizedTypeahead');
-
 const {Typeahead} = require('react-bootstrap-typeahead');
-
 
 class SudebarA extends React.Component {
   constructor() {
@@ -21,25 +23,17 @@ class SudebarA extends React.Component {
   render() {
     return (
       <div>
-          <div className="px-5 py-5">
-       
-      <Typeahead
-          labelKey="name" 
-        options={options}
-        multiple
-          placeholder="Choose a state..."
-        />      
-            
-      </div>
+           <li>
+                  <ButtonGroup vertical block data-toggle="buttons">
+                    <Button className="btn btn-block" onClick={this._onOptionChange.bind(this, 'optionA')} active={this.state.option === 'optionA'}>Option A</Button>
+                    <Button className="btn btn-block" onClick={this._onOptionChange.bind(this, 'optionB')} active={this.state.option === 'optionB'}>Option B</Button>
+                    <Button className="btn btn-block" onClick={this._onOptionChange.bind(this, 'optionC')} active={this.state.option === 'optionC'}>Option C</Button>
+                  </ButtonGroup>
+            </li>
+
          </div>
     );
   }
 }
 module.exports = SudebarA;
 
-/*<CustomizedTypeahead
-          labelKey="name" 
-        options={options}
-        multiple
-          placeholder="Choose a state..."
-        />   */
