9673fd2ca1d2780e936c7cf1877eea12c46c40bb
Glitch (mousy-tiara) <none>
2020-02-27 18:43:59 +0000

    🍦🌷 Checkpoint ./app/components/FilterA.jsx:5890191/635 ./app/components/exampleData.jsx:5890191/4341

--- app/components/FilterA.jsx
+++ app/components/FilterA.jsx
@@ -3,9 +3,36 @@ const Link = require('react-router-dom').Link
 // style for BOOKS
 const style = require('../styles/FormRI');
 // other components and etc
+const options =require('./exampleData');
 const Header = require('./Header');
 //const CreatableSelect= require('react-select');
 //const { Creatable }= require('react-select');
 
-// react-bootstrap
-const {TableHeaderColumn, InputGroup, option, Form, FormGroup, Col, FormControl, Button, Grid, Row, ControlLabel} = require('react-bootstrap');
+// react-bootstrap react-bootstrap-typeahead
+const {Typeahead, TableHeaderColumn, InputGroup, option, Form, FormGroup, Col, FormControl, Button, Grid, Row, ControlLabel} = require('react-bootstrap');
+
+class FilterA extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+            <Header/>
+      <div className="px-5 py-5">
+        <Typeahead
+          labelKey="name"
+          options={options}
+          multiple
+          placeholder="Choose a state..."
+        />
+      </div>
+         </div>
+    );
+  }
+}
+module.exports = FilterA;
