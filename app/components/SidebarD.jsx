0258c3df6bf04447351020ef89ffa8b78f8f49aa
Glitch (mousy-tiara) <none>
2020-03-02 23:29:22 +0000

    🎸🍛 Checkpoint ./app/components/SidebarD.jsx:5890191/3315

--- app/components/SidebarD.jsx
+++ app/components/SidebarD.jsx
@@ -7,137 +7,37 @@ const Link = require('react-router-dom').Link
 const {Alert, FormControlLabel, FormControl, FormGroup, Group, Radio, Col, Grid, Row, Button, Glyphicon } = require('react-bootstrap');
 //const FormikRadioGroup =require('./FormikRadioGroup');
 const style = require('../styles/SidebarA');
-const { Formik,  Field, ErrorMessage } =require('formik');
+const { useFormik, Formik,  Field, ErrorMessage } =require('formik');
 //const options =require('./exampleData');
 const {Yup} =require('yup');
-const style1 = {
-    width: '100%',
-    margin: 'auto'}
-const style2 = {
-    paddingTop: '1em',}
-const style3 = {
-    marginRight: '1em'}
-
-// Render Prop
-//https://stackoverflow.com/questions/55910688/formik-with-react-bootstrap-styling
-//<AlertDismissible />
-class AlertDismissible extends React.Component {
-   
-  constructor(props) {
-     alert("hi2 from AlertDismissible ");
-    super(props);
-
-    this.state = { show: true };
-    
-     alert("hi3 from AlertDismissible ");
-  }
-
-  render() {
-
-    return (
-      <div>
-        <Alert variant='light'>
-          <p>
-            We'll be in touch with login details shortly.
-            </p>
-          <hr />
-          <div className="d-flex justify-content-end">
-            <Button variant="outline-success">
-              Close
-              </Button>
-          </div>
-        </Alert>
-      </div>
-    );
-  }
-}
-
-const Form = (props) => {
-  return (<div>
-    <h1>Anywhere in your app!</h1>
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        let errors = {};
-        
-        return errors;
-      }}
-      onSubmit={props.handleSubmit}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-          <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              name="email"
-             
-              value={values.email}
-            />
-            
-            <input
-              type="password"
-              name="password"
-              //onChange={handleChange}
-              //onBlur={handleBlur}
-              value={values.password}
-            />
-           
-            <button type="submit" >
-              Submit
-          </button>
-          </form>
-        )}
-    </Formik>
-  </div>
-  )
-}
 
 
-class SidebarD extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React',
-      success: false,
-      isLoading: false
-    };
-  }
 
-
-    
-
-     handleSubmit() {
-    this.setState({
-      isLoading: false ,
-      success: true
-    });
-
-    //this.delay()
-     // .then(e => this.setState({ success: true, isLoading: false }))
-      //.catch(e => console.log(e))
-  }
-
-  render() {
-    return (
-      <div>
-        {   //this.state.isLoading ? <p>submit...</p> : this.state.success  ? <AlertDismissible />      : 
-          <Form handleSubmit={this.handleSubmit} />
-        }
-      </div>
-    );
-  }
-}
-  
-  
-  
+const SidebarD= () => {
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 module.exports = SidebarD;
 /* 
