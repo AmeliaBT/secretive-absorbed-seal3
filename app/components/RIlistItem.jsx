95e7d6fb8f6fee1199296d55357343d02183b68d
Glitch (mousy-tiara) <none>
2020-03-04 17:58:16 +0000

    💰🍘 Checkpoint ./app/styles/RIlistItem.css:5890191/211 ./app/components/RIlistItem.jsx:5890191/31

--- app/components/RIlistItem.jsx
+++ app/components/RIlistItem.jsx
@@ -73,7 +73,7 @@ class RIlistItem extends React.Component {
         <Col  sm={1} > {this.state.owo}</Col>
           <Col  sm={3} > {this.state.record}</Col>
         <Col  sm={1} >  <Link to={'report-edit/' + this.state.reportID  }>
-            <Button bsSize="xsmall" ><Glyphicon glyph="edit"/> </Button></Link> </Col>
+            <Button bsSize="xsmall" ><spanG><Glyphicon glyph="edit"/> </spanG></Button></Link> </Col>
        
               
         </Row>
