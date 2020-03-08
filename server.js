158f7406afe4cda10c00d98a4e164b310aff8d84
Glitch (cosmic-peach-f65so9gni) <none>
2020-03-08 23:15:49 +0000

    🍇🗾 Checkpoint ./server.js:5890191/63

--- server.js
+++ server.js
@@ -485,6 +485,7 @@ app.post("/create-filtered-table2", function(request, response) {
 let regGwo = request.body.Gwo; //Model
 let regfwo = request.body.fwo; //PN
 let reg_inspector= request.body.inspector; 
+  if(reg_inspector=== "Other"){reg_inspector='^Tuan'};
    //reportModel.find({"inspector":request.body.inspector}, (err, doc) => {
   reportModel.find({ 
      Gwo: new RegExp(regGwo, 'ig'), 
