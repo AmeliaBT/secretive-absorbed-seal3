aa4334eb92ecb80d2d2b3297c35c5074ff1aae3c
Glitch (cosmic-peach-f65so9gni) <none>
2020-03-10 00:05:29 +0000

    🙈❄️ Checkpoint ./app/components/xSidebarA.jsx:5890191/1387 ./app/components/ChartA.jsx:5890191/2890

---
+++ app/components/ChartA.jsx
@@ -0,0 +1,28 @@
+/* ****************************************
+https://www.npmjs.com/package/react-google-charts
+*************************************************** */
+const React = require('react');
+const Link = require('react-router-dom').Link
+const  { Chart }= require('react-google-charts'); 
+
+
+
+class ChartA extends React.Component {
+  render() {
+    return (
+      <div className={"my-pretty-chart-container"}>
+        <Chart
+          chartType="ScatterChart"
+          data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+          width="100%"
+          height="400px"
+          legendToggle
+        />
+      </div>
+    );
+  }
+}
+  
+  
+module.exports = ChartA;
+
