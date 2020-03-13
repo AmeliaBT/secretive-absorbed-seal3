d4e9aefcbe5e4d1f300cc0bc0bd0924f70a39167
Glitch (cosmic-peach-f65so9gni) <none>
2020-03-13 00:19:12 +0000

    🌯🌷 Checkpoint ./app/components/ChartA.jsx:5890191/38

--- app/components/ChartA.jsx
+++ app/components/ChartA.jsx
@@ -14,7 +14,7 @@ let arrayOfRIs2= [  ["Date", "Lot Size", "Qty Tested", "Qty Fail", "Qty Rejected
 let MyChartRI1;
 let MyChartRI2;
 //let optionsCh2={title: 'Lot Qty '};
-let optionsCh1 ={  title: 'RI Quantity Tested',
+let optionsCh1 ={  title: 'Receiving Inspection',
                timeline: {  groupByRowLabel: true },
                 hAxis: {title: "Date Inspected",  format: 'MMM/yyyy'},
                vAxis: { title: "Lot Size", minValue: 0 },
