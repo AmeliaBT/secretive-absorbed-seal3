b42ea710ca1006aef64fd1697cace53b4dc1e2a7
Glitch (gentle-perfect-pepper) <none>
2020-03-21 18:32:59 +0000

    👁🗃 Checkpoint ./app/components/RIlistAll.jsx:5890191/6103

--- app/components/RIlistAll.jsx
+++ app/components/RIlistAll.jsx
@@ -68,6 +68,7 @@ class RIlistAll extends React.Component {
     // load reports
       let that = this;
       let xhr = new XMLHttpRequest();  
+ 
       xhr.open('POST', '/get-all-users-reports', true);
       xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
       xhr.send();
@@ -77,35 +78,14 @@ class RIlistAll extends React.Component {
           alert( 'error: ' + (this.status ? this.statusText : 'request has not been set') );
           return;
         }
-        let response = JSON.parse(this.responseText); 
+        let response = JSON.parse(this.responseText);
         
-         let reports = response.reports.map((el) => {          
-          if(el.cwo !== ""){
-          let myDate= new Date(el.cwo.substring(0,10));
-           // myDate.format("YYYY/mmm");
-          let myDate2= el.cwo.substring(0,4 ) +"-" +el.cwo.substring(5,7) ; 
-          let myLot= el.owo;            
-          let qtyTested= el.pwo; 
-          let qtyFail= el.qwo; 
-          let qtyRejected= el.rwo; 
-          let pass_fail= el.two; 
-             arrayOfRIs1.push([myDate, myLot ]) ; 
-             arrayOfRIs2.push([myDate,  qtyTested, qtyFail]) ; 
-    // getting unique YYYY-MM
-             arrayOfRIs3.push(myDate2 ) ;            
-           if(pass_fail.length === null){
-           arrayOfRIsPF.push([myDate2, "Fail"]);
-           }else{ arrayOfRIsPF.push([myDate2, pass_fail]);}
-                    
-      } 
-        return     
-        });
-        //for the table   
- 
-       // that.setState( {showTableHead:true});
-      reportsT = response.map((el) => {          
-          return <RIlistItemMonth 
-            key={el.reportID}
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
+             
+        
+         let reports = response.reports.map((el) => {
+          return <RIlistItemAll
+          key={el.reportID}
            reportnumber={el.reportID}  
            inspector={el.inspector}
              fwo={el.fwo}     
@@ -117,65 +97,60 @@ class RIlistAll extends React.Component {
            lwo ={el.lwo}       
             /> 
         });
-   
-         
-      }       
-  
-  Array.prototype.unique = function () {
-  return [...new Set(this)]
-}
-  uniqueYM = arrayOfRIs3.unique();
-  arrLotYM=[["Y-M", "LAR"]]; //[Y-M , LAR (sum of lot  PASS/all lots)
         
-//==========================================   
- for(let i=1; i< uniqueYM.length; i++){ 
-    let lotT=0; //number of lots
-    let lotA=0; //number of lots PASS
-   for(let j=0; j< arrayOfRIsPF.length; j++ ){
-            let ym= arrayOfRIsPF[j][0];            
-          if(ym === uniqueYM[i] ) {
-            lotT= lotT+1;
-            if(arrayOfRIsPF[j][1] === "Pass" ){ lotT =lotA+1;}           
-          }
-   }
-    arrLotYM.push([new Date(uniqueYM[i]), lotA/lotT*100 ]) ;
- }
-            that.setState({
-           ["reports"]: {reports},
-            "arrayOfRIs1" : arrayOfRIs1,
-             "arrayOfRIs2" : arrayOfRIs2,
-             "arrLotYM":arrLotYM,
-            showTableHead:false
-          
+ // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!           
+        
+           that.setState({
+          ["reports"]: <div className="reports">
+                      {reports}     
+                    </div>
            });
-       
-   }
-}
-   
+       }
+    
+  }
+  //https://react-bootstrap.github.io/components/table/
   render() {
     return (
       <div>
         <Header/> 
-  <br/>
-  <Table >  
-    {this.state.showTableHead && <TableHead />}
-    {this.state.reportsT}
+    
+               
+              
+      {/* 
+       < FilterA />   */}   
+      
+
+
+
+    <div   >  
+    
+      
+  
+<Table className="myForm">  
+                 
+            <Row>
+             <Col sm={1}> <b>RI </b>  </Col> 
+                <Col sm={1} ><b> Inspector</b> </Col> 
+                <Col sm={1} ><b> Part Number </b></Col> 
+                <Col sm={2} ><b>Description </b></Col> 
+               <Col  sm={1} ><b> Date Inspected</b> </Col> 
+              <Col  sm={1} ><b>Pass /Fail </b> </Col> 
+               <Col sm={1} > <b>Lot Size </b> </Col> 
+              <Col sm={3} > <b>Comment</b> </Col> 
+               <Col sm={1} > <b>View </b> </Col> 
+           </Row>
+        
+        {this.state.reports}
   </Table>  
-<Modal show={this.state.show} onHide={this.handleClose}>  </Modal>  
+         <Modal show={this.state.show} onHide={this.handleClose}>  </Modal>
+   
   
     </div>
+   
 
-      
+      </div>
     );
   }
 };
 
 module.exports = RIlistAll;
-
-/* 
-
-
-
-
-
-*/
